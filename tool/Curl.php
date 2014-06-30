c9c9cc98a0a6a9463e5f1df260d3d0698da91b05
Grithin <grithin@clrmo.com>
2014-06-30 09:46:57 -0700

    modded doc

--- tool/Curl.php
+++ tool/Curl.php
@@ -1,42 +1,14 @@
 <?
 //Debug with #$curl->options[CURLOPT_VERBOSE] = true;
 
-
-///Handling curl functions
+///Handling curl functionality
 /*
-Copyright (c) 2008 Sean Huber - shuber@huberry.com
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-https://github.com/theshock/curl
+Originaly from
+	2008 Sean Huber - shuber@huberry.com (https://github.com/theshock/curl), 
+	Fabian Grassl
+but I changed a lot
 */
 
-
-
-/**
-* A basic CURL wrapper
-*
-*
-* @author Sean Huber <shuber@huberry.com>
-* @author Fabian Grassl
-**/
 class Curl{
 	public $headers = array();///<Add headers into this variable
 	public $cookie_file = null;///<the file path used for the curl cookie
